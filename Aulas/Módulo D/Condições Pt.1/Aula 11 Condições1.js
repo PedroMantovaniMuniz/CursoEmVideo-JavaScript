c5dfa1f41b423059e8cac8b1c7d0ab81387fa041
Pedro Mantovani <pedromantovani03@gmail.com>
2025-04-19 01:27:40 -0300

    Adding Lesson 11 of Module D

---
+++ Aulas/Módulo D/Condições Pt.1/Aula 11 Condições1.js
@@ -0,0 +1,16 @@
+/*
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if (vel > 60) { // Condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+}
+console.log('Dirija sempre usando cinto de segurança!');
+*/
+
+var país = 'EUA';
+console.log(`Vivendo em ${país}`);
+if (país != 'Brasil') { // Condição composta
+    console.log('Você é Estrangeiro!');
+} else {
+    console.log('Você é Brasileiro!');
+}
