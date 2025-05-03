703a08cee472ad25e89b72978ed96c60aa745ca7
Pedro Mantovani <pedromantovani03@gmail.com>
2025-05-03 17:37:31 -0300

    Adding Lesson 13 of Module E

---
+++ Aulas/Módulo E/Repetições Pt.1/Aula 13 Repetições1.js
@@ -0,0 +1,21 @@
+/*
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+console.log("Hello, World!");
+*/
+
+/*
+var c = 1;
+while (c <= 5) {
+    console.log(`Passo ${c}`);
+    c++;
+}
+*/
+
+var c = 1;
+do {
+    console.log(`Passo ${c}`);
+    c++;
+} while (c <= 5);
